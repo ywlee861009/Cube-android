1e3613c29ab64747336468f9e4d8759ad904e54c
Moon-Metrex <ywlee861009@gmail.com>
2026-04-29 10:08:52 +0900

    deploy 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kero.cubie"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
